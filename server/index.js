1626ead2185f6c381fe1420e5020d9e6f2092f35
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-12 13:39:59 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("we have new connection");
+
+  socket.on("disconnect", () => {
+    console.log("user has left");
+  });
+});
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`server has started on port ${PORT}`));
